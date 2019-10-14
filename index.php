caefa4a89953b80d3cfec33ae7aaba169a733bf2
Alan Espinet <aelluvetaal@gmail.com>
2019-10-14 11:57:26 -0400

    Recaptcha Verification

--- index.php
+++ index.php
@@ -1,10 +1,21 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 
+$url = 'https://www.google.com/recaptcha/api/siteverify';
 $data = array(
-  'name' => 'alan',
-  'age' => 35,
-  'gender' => 'M'
+  'secret' => '6LdDiL0UAAAAAGtvR0k5QvtBBvnwqME3_JKfUvk4',
+  'token' => $_POST['token']
 );
 
-echo json_encode( $data );
+$options = array(
+  'http' => array(
+    'header' => 'Content-type: application/x-www-form-urlencoded\r\n',
+    'method' => 'POST',
+    'content' => http_build_query( $data )
+  )
+);
+
+$context = stream_context_create( $options );
+$response = get_file_contents( $url, false, $context );
+
+echo json_encode( $response );
